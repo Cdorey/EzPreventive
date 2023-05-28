394755ed2593633264b0cdfc33ca735087b71466
bsense <bsense@live.com>
2023-05-28 19:37:50 +0800

    Add personal methods

--- EzNutrition/Server/Data/Entities/Person.cs
+++ EzNutrition/Server/Data/Entities/Person.cs
@@ -5,7 +5,8 @@ namespace EzNutrition.Server.Data.Entities
     /// <summary>
     /// 个案人模型<br/>
     /// 包含7个维度的限定条件：年龄、性别、特殊生理时期、疾病状态、体型、活动强度<br/>
-    /// 使用包括标准模型（树形派生，单一限定条件）和生产模型（菱形派生，关联DRIs数据）<br/>
+    /// 使用包括标准模型（树形派生，单一限定条件）和多条件模型（菱形派生）<br/>
+    /// 框架无法直接判断模型派生的逻辑关系，需要建立时正确添加及维护
     /// </summary>
     public class Person : RecordBase
     {
@@ -68,6 +69,67 @@ namespace EzNutrition.Server.Data.Entities
         public List<MultiDerivedPersonRelationship>? MultiDerivedTo { get; set; }
 
         public List<PersonalDietaryReferenceIntakeValue>? DietaryReferenceIntakes { get; set; }
+        #endregion
+
+        #region Comparators
+        private IEnumerable<Person> GetDerivingTree()
+        {
+            var pointer = this;
+            while (pointer.DerivedFromPerson != null)
+            {
+                yield return pointer.DerivedFromPerson;
+                pointer = pointer.DerivedFromPerson;
+            }
+        }
+
+        public bool IsDerivedFrom(Person person)
+        {
+            foreach (var item in GetDerivingTree())
+            {
+                if (item.PersonId == person.PersonId)
+                    return true;
+            }
+            return false;
+        }
+
+        public int Depth()
+        {
+            return GetDerivingTree().Count();
+        }
+
+        /// <summary>
+        /// 合并两个模型的限定条件，其中+号左边的模型总是优先
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Person operator +(Person a, Person b)
+        {
+            return new Person
+            {
+                PersonId = a.PersonId,
+                AgeStart = a.AgeStart ?? b.AgeStart,
+                AgeEnd = a.AgeEnd ?? b.AgeEnd,
+                Gender = a.Gender ?? b.Gender,
+                SpecialPhysiologicalPeriod = a.SpecialPhysiologicalPeriod ?? b.SpecialPhysiologicalPeriod,
+                Illness = a.Illness ?? b.Illness,
+                BodySize = a.BodySize ?? b.BodySize,
+                PhysicalActivityLevel = a.PhysicalActivityLevel ?? b.PhysicalActivityLevel,
+                Cite = $"Running time print model."
+            };
+        }
+
+        public Person PrintMultiDerivedModel()
+        {
+            var parents = from item in MultiDerivedFrom
+                          select item.ParentModel;
+            var person = new Person();
+            foreach (var item in parents)
+            {
+                person += item;
+            }
+            return person;
+        }
         #endregion
     }
 }
--- EzNutrition/Server/Data/Repositories/PersonalModelRespository.cs
+++ EzNutrition/Server/Data/Repositories/PersonalModelRespository.cs
@@ -24,6 +24,7 @@ namespace EzNutrition.Server.Data.Repositories
             throw new NotImplementedException();
         }
 
+
         public PersonalDietaryReferenceIntakeValue AddDrisRecord()
         {
             throw new NotImplementedException();
