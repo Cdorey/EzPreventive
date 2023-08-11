233630fb253e153b4154ddd1bf486ddccc2541f5
bsense <bsense@live.com>
2023-08-11 21:44:24 +0800

    Add controller for nutrition prescription

---
+++ EzNutrition/Server/Controllers/PrescriptionController.cs
@@ -0,0 +1,39 @@
+﻿using EzNutrition.Server.Data;
+using EzNutrition.Server.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EzNutrition.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PrescriptionController : ControllerBase
+    {
+        private readonly DiseaseRepository _diseaseRepository;
+        private readonly AdviceRepository _adviceRepository;
+
+        [HttpGet("diseases")]
+        public IActionResult GetDiseaseList()
+        {
+            var diseases = _diseaseRepository.GetDiseases();
+            return Ok(diseases);
+        }
+
+        [HttpPost("advices")]
+        public IActionResult GetAdvicesForDiseases([FromBody] List<int> diseaseIDs)
+        {
+            if (diseaseIDs == null || diseaseIDs.Count == 0)
+            {
+                return BadRequest("Disease IDs must be provided.");
+            }
+
+            var advices = _adviceRepository.GetAdviceByDiseaseID(diseaseIDs);
+            return Ok(advices);
+        }
+        public PrescriptionController(EzNutritionDbContext ezNutritionDbContext)
+        {
+            _diseaseRepository = new(ezNutritionDbContext);
+            _adviceRepository = new(ezNutritionDbContext);
+        }
+    }
+
+}
