2513ebb509a82bce35f6bdcea1cc542ae872b663
bsense <bsense@live.com>
2023-08-16 09:07:53 +0800

    Add user center identity

--- EzNutrition/Server/Program.cs
+++ EzNutrition/Server/Program.cs
@@ -1,7 +1,11 @@
 using EzNutrition.Server.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EzNutrition
 {
@@ -12,12 +16,26 @@ namespace EzNutrition
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<EzNutritionDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EzNutritionDB")));
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDb")));
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    //ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    //ValidIssuer = "EzPreventive",
+                    ValidAudience = "EzNutrition",
+                    IssuerSigningKey = new RsaSecurityKey(null)
+                };
+            });
             builder.Services.AddControllersWithViews();
             builder.Services.AddRazorPages();
-            builder.Services.AddDbContext<EzNutritionDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EzNutritionDB")));
-            var app = builder.Build();
 
+            var app = builder.Build();
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -27,6 +45,8 @@ namespace EzNutrition
             {
                 app.UseExceptionHandler("/Error");
             }
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
