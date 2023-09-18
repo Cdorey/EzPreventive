cf7757cef9c15db3ccc6d600de7c29f66fa5ba25
bsense <bsense@live.com>
2023-09-18 21:06:48 +0800

    Import Ant Design package

--- EzNutrition/Client/Program.cs
+++ EzNutrition/Client/Program.cs
@@ -16,6 +16,7 @@ namespace EzNutrition.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<UserSessionService>();
+            builder.Services.AddAntDesign();
 
             await builder.Build().RunAsync();
         }
