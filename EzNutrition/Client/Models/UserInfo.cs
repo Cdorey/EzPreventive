59e10500435cc066abc612fcb40611c78e00e9d7
bsense <bsense@live.com>
2023-09-29 13:47:28 +0800

    Replaced favicon

--- EzNutrition/Client/Models/UserInfo.cs
+++ EzNutrition/Client/Models/UserInfo.cs
@@ -1,7 +1,6 @@
 ﻿using EzNutrition.Shared.Identities;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using static System.Net.WebRequestMethods;
 
 namespace EzNutrition.Client.Models
 {
