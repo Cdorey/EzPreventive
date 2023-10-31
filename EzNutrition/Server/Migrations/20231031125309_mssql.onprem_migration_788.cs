6718281654852602c7da3adcd98cfe4099b051d5
bsense <bsense@live.com>
2023-10-31 20:58:44 +0800

    Added EzNutritionDb Migration

---
+++ EzNutrition/Server/Migrations/20231031125309_mssql.onprem_migration_788.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EzNutrition.Server.Migrations
+{
+    public partial class mssqlonprem_migration_788 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FoodNutrientValues_Nutrients_NutrientId",
+                table: "FoodNutrientValues");
+
+            migrationBuilder.DropTable(
+                name: "MultiDerivedPersonRelationships");
+
+            migrationBuilder.DropTable(
+                name: "PersonalDietaryReferenceIntakes");
+
+            migrationBuilder.DropTable(
+                name: "Nutrients");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.CreateTable(
+                name: "DRIs",
+                columns: table => new
+                {
+                    DietaryReferenceIntakeValueId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AgeStart = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpecialPhysiologicalPeriod = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nutrient = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false),
+                    RecordType = table.Column<int>(type: "int", nullable: false),
+                    IsOffset = table.Column<bool>(type: "bit", nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MeasureUnit = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false),
+                    Detail = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DRIs", x => x.DietaryReferenceIntakeValueId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nutrient",
+                columns: table => new
+                {
+                    NutrientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DefaultMeasureUnit = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FriendlyName = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nutrient", x => x.NutrientId);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FoodNutrientValues_Nutrient_NutrientId",
+                table: "FoodNutrientValues",
+                column: "NutrientId",
+                principalTable: "Nutrient",
+                principalColumn: "NutrientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FoodNutrientValues_Nutrient_NutrientId",
+                table: "FoodNutrientValues");
+
+            migrationBuilder.DropTable(
+                name: "DRIs");
+
+            migrationBuilder.DropTable(
+                name: "Nutrient");
+
+            migrationBuilder.CreateTable(
+                name: "Nutrients",
+                columns: table => new
+                {
+                    NutrientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DefaultMeasureUnit = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FriendlyName = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nutrients", x => x.NutrientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DerivedFromPersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    AgeEnd = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    AgeStart = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    BodySize = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cite = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FriendlyName = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Illness = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhysicalActivityLevel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpecialPhysiologicalPeriod = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.PersonId);
+                    table.ForeignKey(
+                        name: "FK_People_People_DerivedFromPersonId",
+                        column: x => x.DerivedFromPersonId,
+                        principalTable: "People",
+                        principalColumn: "PersonId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MultiDerivedPersonRelationships",
+                columns: table => new
+                {
+                    MultiDerivedPersonRelationshipId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChildModelId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ParentModelId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MultiDerivedPersonRelationships", x => x.MultiDerivedPersonRelationshipId);
+                    table.ForeignKey(
+                        name: "FK_MultiDerivedPersonRelationships_People_ChildModelId",
+                        column: x => x.ChildModelId,
+                        principalTable: "People",
+                        principalColumn: "PersonId");
+                    table.ForeignKey(
+                        name: "FK_MultiDerivedPersonRelationships_People_ParentModelId",
+                        column: x => x.ParentModelId,
+                        principalTable: "People",
+                        principalColumn: "PersonId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalDietaryReferenceIntakes",
+                columns: table => new
+                {
+                    PersonalDietaryReferenceIntakeValueId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NutrientId = table.Column<int>(type: "int", nullable: false),
+                    PersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsOffsetValue = table.Column<bool>(type: "bit", nullable: false),
+                    MeasureUnit = table.Column<string>(type: "varchar(64)", maxLength: 64, nullable: true),
+                    ReferenceType = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalDietaryReferenceIntakes", x => x.PersonalDietaryReferenceIntakeValueId);
+                    table.ForeignKey(
+                        name: "FK_PersonalDietaryReferenceIntakes_Nutrients_NutrientId",
+                        column: x => x.NutrientId,
+                        principalTable: "Nutrients",
+                        principalColumn: "NutrientId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonalDietaryReferenceIntakes_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "PersonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MultiDerivedPersonRelationships_ChildModelId",
+                table: "MultiDerivedPersonRelationships",
+                column: "ChildModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MultiDerivedPersonRelationships_ParentModelId",
+                table: "MultiDerivedPersonRelationships",
+                column: "ParentModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_DerivedFromPersonId",
+                table: "People",
+                column: "DerivedFromPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalDietaryReferenceIntakes_NutrientId",
+                table: "PersonalDietaryReferenceIntakes",
+                column: "NutrientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalDietaryReferenceIntakes_PersonId",
+                table: "PersonalDietaryReferenceIntakes",
+                column: "PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FoodNutrientValues_Nutrients_NutrientId",
+                table: "FoodNutrientValues",
+                column: "NutrientId",
+                principalTable: "Nutrients",
+                principalColumn: "NutrientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- EzNutrition/Server/Migrations/EzNutritionDbContextModelSnapshot.cs
+++ EzNutrition/Server/Migrations/EzNutritionDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace EzNutrition.Server.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.5")
+                .HasAnnotation("ProductVersion", "6.0.21")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
@@ -37,7 +37,7 @@ namespace EzNutrition.Server.Migrations
                     b.ToTable("AdviceDisease");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.Advice", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.Advice", b =>
                 {
                     b.Property<int>("AdviceId")
                         .ValueGeneratedOnAdd()
@@ -56,7 +56,51 @@ namespace EzNutrition.Server.Migrations
                     b.ToTable("Advices");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.Disease", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.DietaryReferenceIntakeValue", b =>
+                {
+                    b.Property<int>("DietaryReferenceIntakeValueId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("DietaryReferenceIntakeValueId"), 1L, 1);
+
+                    b.Property<decimal?>("AgeStart")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.Property<string>("Detail")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Gender")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<bool>("IsOffset")
+                        .HasColumnType("bit");
+
+                    b.Property<string>("MeasureUnit")
+                        .IsRequired()
+                        .HasMaxLength(64)
+                        .HasColumnType("varchar(64)");
+
+                    b.Property<string>("Nutrient")
+                        .IsRequired()
+                        .HasMaxLength(64)
+                        .HasColumnType("varchar(64)");
+
+                    b.Property<int>("RecordType")
+                        .HasColumnType("int");
+
+                    b.Property<string>("SpecialPhysiologicalPeriod")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<decimal>("Value")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.HasKey("DietaryReferenceIntakeValueId");
+
+                    b.ToTable("DRIs");
+                });
+
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.Disease", b =>
                 {
                     b.Property<int>("DiseaseId")
                         .ValueGeneratedOnAdd()
@@ -75,7 +119,7 @@ namespace EzNutrition.Server.Migrations
                     b.ToTable("Diseases");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.EER", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.EER", b =>
                 {
                     b.Property<int>("EERId")
                         .ValueGeneratedOnAdd()
@@ -103,7 +147,7 @@ namespace EzNutrition.Server.Migrations
                     b.ToTable("EERs");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.Food", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.Food", b =>
                 {
                     b.Property<Guid>("FoodId")
                         .ValueGeneratedOnAdd()
@@ -130,7 +174,7 @@ namespace EzNutrition.Server.Migrations
                     b.ToTable("Foods");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.FoodNutrientValue", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.FoodNutrientValue", b =>
                 {
                     b.Property<int>("FoodNutrientValueId")
                         .ValueGeneratedOnAdd()
@@ -163,28 +207,7 @@ namespace EzNutrition.Server.Migrations
                     b.ToTable("FoodNutrientValues");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.MultiDerivedPersonRelationship", b =>
-                {
-                    b.Property<Guid>("MultiDerivedPersonRelationshipId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<Guid?>("ChildModelId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<Guid?>("ParentModelId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.HasKey("MultiDerivedPersonRelationshipId");
-
-                    b.HasIndex("ChildModelId");
-
-                    b.HasIndex("ParentModelId");
-
-                    b.ToTable("MultiDerivedPersonRelationships");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.Nutrient", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.Nutrient", b =>
                 {
                     b.Property<int>("NutrientId")
                         .ValueGeneratedOnAdd()
@@ -207,122 +230,33 @@ namespace EzNutrition.Server.Migrations
 
                     b.HasKey("NutrientId");
 
-                    b.ToTable("Nutrients");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.Person", b =>
-                {
-                    b.Property<Guid>("PersonId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<decimal?>("AgeEnd")
-                        .HasColumnType("decimal(18,2)");
-
-                    b.Property<decimal?>("AgeStart")
-                        .HasColumnType("decimal(18,2)");
-
-                    b.Property<string>("BodySize")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Cite")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<Guid?>("DerivedFromPersonId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<string>("Details")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("FriendlyName")
-                        .IsRequired()
-                        .HasMaxLength(64)
-                        .HasColumnType("varchar(64)");
-
-                    b.Property<string>("Gender")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Illness")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("PhysicalActivityLevel")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("SpecialPhysiologicalPeriod")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("PersonId");
-
-                    b.HasIndex("DerivedFromPersonId");
-
-                    b.ToTable("People");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.PersonalDietaryReferenceIntakeValue", b =>
-                {
-                    b.Property<int>("PersonalDietaryReferenceIntakeValueId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("PersonalDietaryReferenceIntakeValueId"), 1L, 1);
-
-                    b.Property<string>("Details")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<bool>("IsOffsetValue")
-                        .HasColumnType("bit");
-
-                    b.Property<string>("MeasureUnit")
-                        .HasMaxLength(64)
-                        .HasColumnType("varchar(64)");
-
-                    b.Property<int>("NutrientId")
-                        .HasColumnType("int");
-
-                    b.Property<Guid>("PersonId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<string>("ReferenceType")
-                        .IsRequired()
-                        .HasMaxLength(64)
-                        .HasColumnType("nvarchar(64)");
-
-                    b.Property<decimal>("Value")
-                        .HasColumnType("decimal(18,2)");
-
-                    b.HasKey("PersonalDietaryReferenceIntakeValueId");
-
-                    b.HasIndex("NutrientId");
-
-                    b.HasIndex("PersonId");
-
-                    b.ToTable("PersonalDietaryReferenceIntakes");
+                    b.ToTable("Nutrient");
                 });
 
             modelBuilder.Entity("AdviceDisease", b =>
                 {
-                    b.HasOne("EzNutrition.Shared.Advice", null)
+                    b.HasOne("EzNutrition.Shared.Data.Entities.Advice", null)
                         .WithMany()
                         .HasForeignKey("AdvicesAdviceId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("EzNutrition.Shared.Disease", null)
+                    b.HasOne("EzNutrition.Shared.Data.Entities.Disease", null)
                         .WithMany()
                         .HasForeignKey("DiseasesDiseaseId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.FoodNutrientValue", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.FoodNutrientValue", b =>
                 {
-                    b.HasOne("EzNutrition.Shared.Food", "Food")
+                    b.HasOne("EzNutrition.Shared.Data.Entities.Food", "Food")
                         .WithMany("FoodNutrientValues")
                         .HasForeignKey("FoodId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("EzNutrition.Shared.Nutrient", "Nutrient")
+                    b.HasOne("EzNutrition.Shared.Data.Entities.Nutrient", "Nutrient")
                         .WithMany("FoodNutrientValues")
                         .HasForeignKey("NutrientId")
                         .OnDelete(DeleteBehavior.Cascade)
@@ -333,66 +267,14 @@ namespace EzNutrition.Server.Migrations
                     b.Navigation("Nutrient");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.MultiDerivedPersonRelationship", b =>
-                {
-                    b.HasOne("EzNutrition.Shared.Person", "ChildModel")
-                        .WithMany("MultiDerivedFrom")
-                        .HasForeignKey("ChildModelId");
-
-                    b.HasOne("EzNutrition.Shared.Person", "ParentModel")
-                        .WithMany("MultiDerivedTo")
-                        .HasForeignKey("ParentModelId");
-
-                    b.Navigation("ChildModel");
-
-                    b.Navigation("ParentModel");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.Person", b =>
-                {
-                    b.HasOne("EzNutrition.Shared.Person", "DerivedFromPerson")
-                        .WithMany()
-                        .HasForeignKey("DerivedFromPersonId");
-
-                    b.Navigation("DerivedFromPerson");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.PersonalDietaryReferenceIntakeValue", b =>
-                {
-                    b.HasOne("EzNutrition.Shared.Nutrient", "Nutrient")
-                        .WithMany()
-                        .HasForeignKey("NutrientId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("EzNutrition.Shared.Person", "Person")
-                        .WithMany("DietaryReferenceIntakes")
-                        .HasForeignKey("PersonId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Nutrient");
-
-                    b.Navigation("Person");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.Food", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.Food", b =>
                 {
                     b.Navigation("FoodNutrientValues");
                 });
 
-            modelBuilder.Entity("EzNutrition.Shared.Nutrient", b =>
+            modelBuilder.Entity("EzNutrition.Shared.Data.Entities.Nutrient", b =>
                 {
                     b.Navigation("FoodNutrientValues");
-                });
-
-            modelBuilder.Entity("EzNutrition.Shared.Person", b =>
-                {
-                    b.Navigation("DietaryReferenceIntakes");
-
-                    b.Navigation("MultiDerivedFrom");
-
-                    b.Navigation("MultiDerivedTo");
                 });
 #pragma warning restore 612, 618
         }
