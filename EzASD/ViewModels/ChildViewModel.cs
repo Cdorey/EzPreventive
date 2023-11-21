ae0c9c7fbf1c147c6d904229d482ae9d429bdcb1
bsense <bsense@live.com>
2023-11-21 21:35:07 +0800

    ASD: Added Basic family info form.

--- EzASD/ViewModels/ChildViewModel.cs
+++ EzASD/ViewModels/ChildViewModel.cs
@@ -100,5 +100,76 @@ namespace EzASD.ViewModels
         }
 
         public EarlyWarningViewModel EarlyWarningQes { get; } = new();
+
+        #region FamilyMembers
+
+        private bool isFatherInFamily = true;
+
+        public bool IsFatherInFamily { get => isFatherInFamily; set => SetProperty(ref isFatherInFamily, value); }
+
+        private bool isMotherInFamily = true;
+
+        public bool IsMotherInFamily { get => isMotherInFamily; set => SetProperty(ref isMotherInFamily, value); }
+
+        private bool isGrandpaInFamily = false;
+
+        public bool IsGrandpaInFamily { get => isGrandpaInFamily; set => SetProperty(ref isGrandpaInFamily, value); }
+
+        private bool isGrandmaInFamily = false;
+
+        public bool IsGrandmaInFamily { get => isGrandmaInFamily; set => SetProperty(ref isGrandmaInFamily, value); }
+
+        private bool isNurseInFamily = false;
+
+        public bool IsNurseInFamily { get => isNurseInFamily; set => SetProperty(ref isNurseInFamily, value); }
+
+        private string othersInFamily = string.Empty;
+
+        public string OthersInFamily { get => othersInFamily; set => SetProperty(ref othersInFamily, value); }
+
+        private int fatherEduLevel;
+        #endregion
+
+        public int FatherEduLevel { get => fatherEduLevel; set => SetProperty(ref fatherEduLevel, value); }
+
+        private int motherEduLevel;
+
+        public int MotherEduLevel { get => motherEduLevel; set => SetProperty(ref motherEduLevel, value); }
+
+        private int fatherProfession;
+
+        public int FatherProfession { get => fatherProfession; set => SetProperty(ref fatherProfession, value); }
+
+        private int motherProfession;
+
+        public int MotherProfession { get => motherProfession; set => SetProperty(ref motherProfession, value); }
+
+        private int parentsRelationship;
+
+        public int ParentsRelationship { get => parentsRelationship; set => SetProperty(ref parentsRelationship, value); }
+
+        private int fatherCharacter;
+
+        public int FatherCharacter { get => fatherCharacter; set => SetProperty(ref fatherCharacter, value); }
+
+        private int motherCharacter;
+
+        public int MotherCharacter { get => motherCharacter; set => SetProperty(ref motherCharacter, value); }
+
+        private int primaryEducator;
+
+        public int PrimaryEducator { get => primaryEducator; set => SetProperty(ref primaryEducator, value); }
+
+        private int mainEducationMethods;
+
+        public int MainEducationMethods { get => mainEducationMethods; set => SetProperty(ref mainEducationMethods, value); }
+
+        private int respondent;
+
+        public int Respondent { get => respondent; set => SetProperty(ref respondent, value); }
+
+        private int timeTogether;
+
+        public int TimeTogether { get => timeTogether; set => SetProperty(ref timeTogether, value); }
     }
 }
