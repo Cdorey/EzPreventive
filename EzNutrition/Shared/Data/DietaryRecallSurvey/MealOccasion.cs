a57dd33a20ef10d54c4621844cf52d0208cc8198
bsense <bsense@live.com>
2025-02-13 15:18:37 +0800

    Adjust ui layout

--- EzNutrition/Shared/Data/DietaryRecallSurvey/MealOccasion.cs
+++ EzNutrition/Shared/Data/DietaryRecallSurvey/MealOccasion.cs
@@ -1,12 +1,21 @@
-﻿namespace EzNutrition.Shared.Data.DietaryRecallSurvey
+﻿using System.ComponentModel.DataAnnotations;
+using System.Resources;
+
+namespace EzNutrition.Shared.Data.DietaryRecallSurvey
 {
     public enum MealOccasion
     {
-        Breakfast = 0,
+        [Display(Name ="早餐")]
+        Breakfast = 1,
+        [Display(Name = "上午")]
         MorningSnack = 2,
+        [Display(Name = "午餐")]
         Lunch = 4,
+        [Display(Name = "下午")]
         AfternoonSnack = 8,
+        [Display(Name = "晚餐")]
         Dinner = 16,
+        [Display(Name = "宵夜")]
         LateNightSnack = 32,
     }
 }
