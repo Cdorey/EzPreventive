27917c3dd6d1900a8bca1784111596d27904047b
bsense <bsense@live.com>
2025-02-22 22:31:12 +0800

    Remove useless code

---
+++ EzNutrition/Server/Extension/ProgramExtension.cs
@@ -0,0 +1,50 @@
+﻿using EzNutrition.Server.Data;
+using EzNutrition.Shared.Policies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+
+namespace EzNutrition.Server.Extension
+{
+    internal static class ProgramExtension
+    {
+        internal static void AuthorizeConfiguration(this WebApplicationBuilder builder)
+        {
+            //Identity and Auth
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+            builder.Services.AddAuthorization(PolicyList.RegisterPolicies);
+            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+            var publicKeyBytes = Convert.FromBase64String(builder.Configuration["JwtSettings:PublicKey"]!);
+            var rsa = RSA.Create();
+            rsa.ImportSubjectPublicKeyInfo(publicKeyBytes, out _);
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "EzPreventive",
+                    ValidAudience = "EzNutrition",
+                    IssuerSigningKey = new RsaSecurityKey(rsa)
+                };
+            });
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            });
+        }
+    }
+}
--- EzNutrition/Server/Program.cs
+++ EzNutrition/Server/Program.cs
@@ -1,9 +1,8 @@
 using EzNutrition.Server.Data;
 using EzNutrition.Server.Data.Repositories;
+using EzNutrition.Server.Extension;
 using EzNutrition.Server.Services;
 using EzNutrition.Server.Services.Settings;
-using EzNutrition.Shared.Policies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.ResponseCompression;
@@ -11,10 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Cryptography;
 
 namespace EzNutrition.Server
 {
@@ -77,48 +73,6 @@ namespace EzNutrition.Server
             app.MapFallbackToFile("index.html");
 
             app.Run();
-        }
-    }
-
-    internal static class ProgramExtension
-    {
-        internal static void AuthorizeConfiguration(this WebApplicationBuilder builder)
-        {
-            #region Identity and Auth
-            //Identity and Auth
-            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-            builder.Services.AddAuthorization(PolicyList.RegisterPolicies);
-            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
-            var publicKeyBytes = Convert.FromBase64String(builder.Configuration["JwtSettings:PublicKey"]!);
-            var rsa = RSA.Create();
-            rsa.ImportSubjectPublicKeyInfo(publicKeyBytes, out _);
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = "EzPreventive",
-                    ValidAudience = "EzNutrition",
-                    IssuerSigningKey = new RsaSecurityKey(rsa)
-                };
-            });
-            builder.Services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequireDigit = false;
-                options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireLowercase = false;
-            });
-            #endregion
         }
     }
 }
